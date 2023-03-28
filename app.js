a1f6405cd60dd78d035710c47d8db01a7d401200
Zipeth <markchristensen1010@gmail.com>
2023-03-28 13:27:15 +0100

    my attempt at task 5
    '

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {getCategories} = require("./Controllers/categories_controllers");
-const {getReviewById} = require("./Controllers/Reviews_controller")
+const {getReviewById, getReviews} = require("./Controllers/Reviews_controller")
 const db = require("./db/connection");
 const { handle400Errors, handleCustomErrors } = require("./error_handlers");
 
@@ -9,6 +9,8 @@ const app = express();
 app.get("/api/categories", getCategories);
 
 app.get("/api/reviews/:review_id", getReviewById)
+
+app.get("/api/reviews", getReviews)
 
 app.use("/*", (req, res) => {
     res.status(404).send({msg: "Path not found"})
