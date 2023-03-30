1f67923b79e7decb402fa968bc4de5525fecc5e6
Zipeth <markchristensen1010@gmail.com>
2023-03-30 12:00:59 +0100

    accidentaly worked a little on main

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -290,6 +290,26 @@ describe("DELETE /api/comments/:comment_id", () => {
     })
 })
 
+// describe("GET /api/users", () => {
+//   test("200: returns an array of objects each object having username, name and avatar_url properties", () => {
+//     return request(app)
+//     .get("/api/users")
+//     .expect(200)
+//     .then(({body}) => {
+//       expect(body.users).toHaveLength(4)
+//       expect(body.users).toBeInstanceOf(Array)
+//       body.users.forEach((user) => {
+//         expect(user).toMatchObject({
+//           username: expect.any(String),
+//           name: expect.any(String),
+//           avatar_url: expect.any(String)
+//         })
+//       })
+//       })
+//     })
+//   })
+
+
 
 describe("GET /*", () => {
   test("404: If there is an error with spelling, the response is 404 with the message 'Path not found'.", () => {
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const db = require("./db/connection");
 const { handle400Errors, handleCustomErrors, handlePSQLErrors } = require("./error_handlers");
 const {getCommentById, postCommentById, deleteComment} = require("./Controllers/comments_controller")
 const {updateVotes} = require("./Controllers/Reviews_controller")
+// const {getUsers} = require("./Controllers/Users_controller")
 
 const app = express();
 
@@ -23,6 +24,8 @@ app.post("/api/reviews/:review_id/comments", postCommentById)
 app.patch("/api/reviews/:review_id", updateVotes)
 
 app.delete("/api/comments/:comment_id", deleteComment)
+
+// app.get("/api/users", getUsers)
 
 app.use("/*", (req, res) => {
     res.status(404).send({msg: "Path not found"})
