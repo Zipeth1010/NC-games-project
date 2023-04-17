c75ee1b0e5fb91b42d3a4ad17bb1c985d5954933
Zipeth <markchristensen1010@gmail.com>
2023-04-17 10:55:52 +0100

    cors added

--- app.js
+++ app.js
@@ -1,40 +1,52 @@
 const express = require("express");
-const {getCategories} = require("./Controllers/categories_controllers");
-const {getReviewById, getReviews} = require("./Controllers/Reviews_controller")
+const { getCategories } = require("./Controllers/categories_controllers");
+const {
+  getReviewById,
+  getReviews,
+} = require("./Controllers/Reviews_controller");
 const db = require("./db/connection");
-const { handle400Errors, handleCustomErrors, handlePSQLErrors } = require("./error_handlers");
-const {getCommentById, postCommentById, deleteComment} = require("./Controllers/comments_controller")
-const {updateVotes} = require("./Controllers/Reviews_controller")
-const {getUsers} = require("./Controllers/Users_controller")
+const {
+  handle400Errors,
+  handleCustomErrors,
+  handlePSQLErrors,
+} = require("./error_handlers");
+const {
+  getCommentById,
+  postCommentById,
+  deleteComment,
+} = require("./Controllers/comments_controller");
+const { updateVotes } = require("./Controllers/Reviews_controller");
+const { getUsers } = require("./Controllers/Users_controller");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json())
+app.use(cors());
+
+app.use(express.json());
 
 app.get("/api/categories", getCategories);
 
-app.get("/api/reviews/:review_id", getReviewById)
+app.get("/api/reviews/:review_id", getReviewById);
 
-app.get("/api/reviews", getReviews)
+app.get("/api/reviews", getReviews);
 
-app.get("/api/reviews/:review_id/comments", getCommentById)
+app.get("/api/reviews/:review_id/comments", getCommentById);
 
-app.post("/api/reviews/:review_id/comments", postCommentById)
+app.post("/api/reviews/:review_id/comments", postCommentById);
 
-app.patch("/api/reviews/:review_id", updateVotes)
+app.patch("/api/reviews/:review_id", updateVotes);
 
-app.delete("/api/comments/:comment_id", deleteComment)
+app.delete("/api/comments/:comment_id", deleteComment);
 
-app.get("/api/users", getUsers)
+app.get("/api/users", getUsers);
 
 app.use("/*", (req, res) => {
-    res.status(404).send({msg: "Path not found"})
-})
+  res.status(404).send({ msg: "Path not found" });
+});
 
-app.use(handle400Errors)
-app.use(handlePSQLErrors)
-app.use(handleCustomErrors)
+app.use(handle400Errors);
+app.use(handlePSQLErrors);
+app.use(handleCustomErrors);
 
-
-module.exports = { app }
-
+module.exports = { app };
