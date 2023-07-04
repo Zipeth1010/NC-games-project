145ebe7b6837d06ac2c2e2c2a92829938ef42ce9
Zipeth <markchristensen1010@gmail.com>
2023-07-04 14:19:18 +0100

    patching comments logic which is fully functional

--- Controllers/comments_controller.js
+++ Controllers/comments_controller.js
@@ -1,47 +1,76 @@
-const {getCommentByIdModel, checkIfIdExists, insertComment, deleteCommentModel, checkIfCommentIdExists} = require("../Models/comments_models")
-
+const {
+  getCommentByIdModel,
+  checkIfIdExists,
+  insertComment,
+  deleteCommentModel,
+  checkIfCommentIdExists,
+  patchCommentModel,
+} = require("../Models/comments_models");
 
 function getCommentById(req, res, next) {
-    const {review_id} = req.params
+  const { review_id } = req.params;
 
-    getCommentByIdModel(review_id).then((comments) => {
-        if (comments.length === 0) {
-            return checkIfIdExists(review_id)
-        }
-        res.status(200).send({comments: comments})
+  getCommentByIdModel(review_id)
+    .then((comments) => {
+      if (comments.length === 0) {
+        return checkIfIdExists(review_id);
+      }
+      res.status(200).send({ comments: comments });
     })
     .then(() => {
-        res.status(200).send({comments: []})
+      res.status(200).send({ comments: [] });
     })
     .catch((err) => {
-        next(err)
-    })
+      next(err);
+    });
 }
 
 function postCommentById(req, res, next) {
-    const {username, body} = req.body
-    const {review_id} = req.params
+  const { username, body } = req.body;
+  const { review_id } = req.params;
 
-    insertComment(username, body, review_id).then((comment) => {
-         res.status(201).send({comment: comment})
+  insertComment(username, body, review_id)
+    .then((comment) => {
+      res.status(201).send({ comment: comment });
     })
     .catch((err) => {
-        next(err)
-    })
+      next(err);
+    });
 }
 
 function deleteComment(req, res, next) {
-    const comment_id = req.params.comment_id
+  const comment_id = req.params.comment_id;
 
-    checkIfCommentIdExists(comment_id).then(() => {
-        deleteCommentModel(comment_id).then(() => {   
-            res.status(204).send()
-        })
+  checkIfCommentIdExists(comment_id)
+    .then(() => {
+      deleteCommentModel(comment_id).then(() => {
+        res.status(204).send();
+      });
     })
     .catch((err) => {
-        next(err)
-    })
-    
+      next(err);
+    });
 }
 
-module.exports = {getCommentById, postCommentById, deleteComment}
+function patchComment(req, res, next) {
+  const comment_id = req.params.comment_id;
+
+  const votes = req.body["inc_votes"];
+
+  checkIfCommentIdExists(comment_id)
+    .then(() => {
+      patchCommentModel(comment_id, votes).then((updatedComment) => {
+        res.status(200).send({ updatedComment: updatedComment });
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+module.exports = {
+  getCommentById,
+  postCommentById,
+  deleteComment,
+  patchComment,
+};
--- Models/comments_models.js
+++ Models/comments_models.js
@@ -1,57 +1,93 @@
-const db = require("../db/connection")
-const format = require('pg-format')
-
+const db = require("../db/connection");
+const format = require("pg-format");
 
 function getCommentByIdModel(review_id) {
-    return db.query(`
+  return db
+    .query(
+      `
     SELECT * FROM comments
-    WHERE review_id = $1;`, [review_id])
+    WHERE review_id = $1;`,
+      [review_id]
+    )
     .then((result) => {
-        return result.rows
-    })
+      return result.rows;
+    });
 }
 
-function checkIfIdExists(review_id){
-    return db.query(`
-    SELECT * FROM reviews WHERE review_id = $1`, [review_id])
+function checkIfIdExists(review_id) {
+  return db
+    .query(
+      `
+    SELECT * FROM reviews WHERE review_id = $1`,
+      [review_id]
+    )
     .then((result) => {
-        if (result.rowCount === 0) {
-            return Promise.reject({status:404, msg: "ID not found"})
-        } 
-    })
+      if (result.rowCount === 0) {
+        return Promise.reject({ status: 404, msg: "ID not found" });
+      }
+    });
 }
 
 function insertComment(username, body, review_id) {
-    return db.query(`
+  return db
+    .query(
+      `
     INSERT INTO comments
     (author, body, review_id)
     VALUES
     ($1, $2, $3)
-    RETURNING *;`, [username, body, review_id])
+    RETURNING *;`,
+      [username, body, review_id]
+    )
     .then((result) => {
-        return result.rows[0]
-    })
+      return result.rows[0];
+    });
 }
 
 function deleteCommentModel(comment_id) {
-    return db.query(`
+  return db
+    .query(
+      `
     DELETE FROM comments
     WHERE comment_id = $1
-    RETURNING *;`, [comment_id])
+    RETURNING *;`,
+      [comment_id]
+    )
     .then((result) => {
-        return result.rows
-    })
+      return result.rows;
+    });
 }
 
-function checkIfCommentIdExists(comment_id){
-    return db.query(`
-    SELECT * FROM comments WHERE comment_id = $1`, [comment_id])
+function checkIfCommentIdExists(comment_id) {
+  return db
+    .query(
+      `
+    SELECT * FROM comments WHERE comment_id = $1`,
+      [comment_id]
+    )
     .then((result) => {
-        if (result.rowCount === 0){
-            return Promise.reject({status:404, msg: "Comment_id not found"})
-        }
-    })
+      if (result.rowCount === 0) {
+        return Promise.reject({ status: 404, msg: "Comment_id not found" });
+      }
+    });
 }
 
+function patchCommentModel(comment_id, votes) {
+  return db
+    .query(
+      `UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *;`,
+      [votes, comment_id]
+    )
+    .then((result) => {
+      return result.rows[0];
+    });
+}
 
-module.exports = {getCommentByIdModel, checkIfIdExists, insertComment, deleteCommentModel, checkIfCommentIdExists}
+module.exports = {
+  getCommentByIdModel,
+  checkIfIdExists,
+  insertComment,
+  deleteCommentModel,
+  checkIfCommentIdExists,
+  patchCommentModel,
+};
